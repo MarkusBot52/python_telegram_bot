0e62955f2fa4df5a6a1accef78d175826781922c
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-05-23 15:59:52 +0100

    Fix

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@ from aiogram.filters import Command
 from aiogram.types import Message
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 BOT_TOKEN = os.getenv('SECRET_KEY')
-
 
 # Создаем объекты бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
